a7819196f8d0b18490c538e592453234cc5e5b44
Mo Amir Khan <104264342+AmirKhancoder@users.noreply.github.com>
2023-05-15 12:26:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+touristSpots.sort(function(a, b) {
+  // Function to remove articles from the strings
+  function removeArticle(str) {
+    return str.replace(/^(a|an|the)\s+/i, '');
+  }
+  
+  // Remove articles and compare the modified strings
+  const modifiedA = removeArticle(a);
+  const modifiedB = removeArticle(b);
+  
+  return modifiedA.localeCompare(modifiedB);
+});
+
+console.log(touristSpots);
